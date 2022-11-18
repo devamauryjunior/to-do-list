081ad43a6525284036c5f5fb57a1aab050d07740
devamauryjunior <jobagpj@gmail.com>
2022-11-18 01:28:40 -0300

    feat: tarefa adicionando corretamente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,50 @@
 const botaoParaAdicionarTarefa = document.querySelector('#addTarefa');
 const inputTarefa = document.querySelector('#tarefa');
 const boxQueReceberarElementoFilho = document.querySelector('.box-2');
+const boxQueReceberarTarefasConcluidas = document.querySelector('.box-3');
+let tarefasConcluidas = [];
+
+document.body.style.overflow = 'hidden';
 
 function addElemento() {
+    let li = document.createElement('li');
     let span = document.createElement('span');
+    li.setAttribute('class', 'modificar');
     span.setAttribute("class", "material-symbols-outlined");
-    span.setAttribute("title", "Excluir");
-    span.textContent = inputTarefa.value.concat(" delete");
-    // boxQueReceberarElementoFilho.style.borderBottom = "1px solid #333333";
-    boxQueReceberarElementoFilho.appendChild(span);
+    span.setAttribute("title", "Excluir tarefa");
+    li.textContent = inputTarefa.value;
+    tarefasConcluidas.push(inputTarefa.value)
+    li.appendChild(span);
+    span.textContent = 'delete';
+    boxQueReceberarElementoFilho.appendChild(li);
 }
+
+// function addTarefaConcluida() {
+//     let li = document.createElement('li');
+//     tarefasConcluidas.forEach(tarefa => {
+//         li.textContent = tarefa.unshift();
+//     })
+//     boxQueReceberarTarefasConcluidas.appendChild(li);
+// }
 
 function inputEstaVazio() {
     if(inputTarefa.value === "") {
         inputTarefa.style.border = '1px solid red';
         inputTarefa.placeholder = 'Valor inválido';
     } else addElemento();
-    
-
 }
 
 botaoParaAdicionarTarefa.addEventListener('click', function () {
     inputEstaVazio();
-    
-})
+});
+
+inputTarefa.addEventListener('focus', () => inputTarefa.value = " ")
+
+document.onclick = event => {
+    if(event.target.classList.value === 'material-symbols-outlined') {
+        event.target.parentNode.remove();
+        // addTarefaConcluida();
+    };
+}
+
+// Objetivo principal concluído, falta só mostrar as tarefas concluídas
